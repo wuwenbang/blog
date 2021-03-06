5a901615c1afef7ffa12ffcda248236559b8ca6e
wuwenbang <wuwenbang@foxmail.com>
2021-03-07 01:38:57 +0800

    init project

---
+++ components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// import '../styles/components/header.css'
+import { Row, Col, Menu } from 'antd'
+import {
+    HomeOutlined,
+    SmileOutlined,
+    YoutubeOutlined
+} from '@ant-design/icons';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Row justify="space-between" type="flex">
+                <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+                    <span className="header-logo">Planet Blog</span>
+                    <span className="header-txt">Hello Earth Man</span>
+                </Col>
+                <Col className="header-menu" xs={0} sm={0} md={14} lg={8} xl={6} >
+                    <Menu mode="horizontal">
+                        <Menu.Item>
+                            <HomeOutlined />
+                            <span>首页</span>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <YoutubeOutlined />
+                            <span>视频</span>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <SmileOutlined />
+                            <span>生活</span>
+                        </Menu.Item>
+                    </Menu>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import 'antd/dist/antd.css'
+import '../styles/pages/common.css'
+import '../styles/components/header.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default App
