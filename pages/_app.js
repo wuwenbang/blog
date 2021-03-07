c03e7f20d9d32c2ecc0e7919857c8ffce897d9fb
wuwenbang <wuwenbang@foxmail.com>
2021-03-07 16:08:08 +0800

    feat 完成List组件基本样式

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App from 'next/app'
 import 'antd/dist/antd.css'
 import '../styles/pages/common.css'
+import '../styles/pages/list.css'
 import '../styles/components/header.css'
 
 export default App
