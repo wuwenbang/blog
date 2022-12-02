7e43c30083b6f808484d1bc1b55b40c6002cc27d
kakarot <wuwenbang@foxmail.com>
2022-12-03 03:29:13 +0800

    build: set basic config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias['src'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+  
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'src/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kakarot Blog</title>
+        <meta name="description" content="Kakarot's blog" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
